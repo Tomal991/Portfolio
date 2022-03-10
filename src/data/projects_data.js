c6885373f637753b1491c7eece2ab940607e5816
Tomal991 <intesarulhaquey4@gmail.com>
2022-03-11 00:08:51 +0600

    update project Info

--- src/data/projects_data.js
+++ src/data/projects_data.js
@@ -3,48 +3,69 @@ export const projectData = [
   {
     id: 1,
     image: mypic,
-    title: "Exam Portal",
-    github: "http://github.com/Tomal991",
-    live: "http://github.com",
+    title: "E-learn System in React and Node JS",
+    github: "https://github.com/rashedabir/E-Learn_System/tree/tomal",
+    live: "https://elearn-bd.web.app/",
   },
 
   {
     id: 2,
     image: mypic,
-    title: "Exam Portal",
-    github: "http://github.com/Tomal991",
+    title: "Exam Portal in Spring Boot and Angular",
+    github: "https://github.com/Tomal991/ExamPortal",
     live: "http://github.com",
   },
-
   {
     id: 3,
     image: mypic,
-    title: "Exam Portal",
-    github: "http://github.com/Tomal991",
+    title: "Hotel Management System in Java",
+    github: "https://github.com/Tomal991/HotelManagementSystem",
     live: "http://github.com",
   },
 
   {
     id: 4,
     image: mypic,
-    title: "Exam Portal",
-    github: "http://github.com/Tomal991",
+    title: "Pig Game with Javascript",
+    github: "https://github.com/Tomal991/pig-game-js",
     live: "http://github.com",
   },
 
   {
     id: 5,
     image: mypic,
-    title: "Exam Portal",
-    github: "http://github.com/Tomal991",
-    live: "http://github.com",
+    title: "Github Profile Finder in JS",
+    github: "https://github.com/Tomal991/Github-profile-finder",
+    live: "https://tomal991.github.io/Github-profile-finder/",
   },
 
   {
     id: 6,
     image: mypic,
-    title: "Exam Portal",
-    github: "http://github.com/Tomal991",
+    title: "Voice Assistant System in Python",
+    github: "https://github.com/Tomal991/AI-Desktop-Voice-Assistant",
+    live: "http://github.com",
+  },
+
+  {
+    id: 7,
+    image: mypic,
+    title: "Starbucks Landing Page in Html and Css",
+    github: "https://github.com/Tomal991/Starbucks-landing-page",
+    live: "https://tomal991.github.io/Starbucks-landing-page/",
+  },
+  {
+    id: 8,
+    image: mypic,
+    title: "Heart Disease Prediction in Machine Learning",
+    github: "https://github.com/Tomal991/Heart_Disease_Prediction-machine-learning",
+    live: "http://github.com",
+  },
+  {
+    id: 9,
+    image: mypic,
+    title: "Food Order System in Android",
+    github: "https://github.com/Tomal991/Food-Order-android-App",
     live: "http://github.com",
   },
 ];
