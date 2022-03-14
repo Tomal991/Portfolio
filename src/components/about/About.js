4a150777310ee6a2b1f7e143a32fc4c274d59a27
Tomal991 <intesarulhaquey4@gmail.com>
2022-03-14 22:00:06 +0600

    about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,30 +8,54 @@ const About = () => {
   return (
     <section id="about">
       <h2 className="title">About Me</h2>
+      <div className="title">
+        <h3>Education</h3>
+        <h3>Experience</h3>
+      </div>
+
       <div className="container about__container">
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <FaAward className="about__icon" />
-              <h5>Experience</h5>
-              <small>1+ years working</small>
-            </article>
-            <article className="about__card">
-              <AiOutlineUser className="about__icon" />
-              <h5>Experience</h5>
-              <small>1+ years working</small>
-            </article>
-            <article className="about__card">
-              <FaFolder className="about__icon" />
-              <h5>Experience</h5>
-              <small>1+ years working</small>
-            </article>
+        <div class="timeline">
+          <div class="contain right">
+            <div class="content">
+              <h4>B.S.C</h4>
+              <h5>Daffodil International University</h5>
+            </div>
           </div>
 
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste,
-            quaerat?
-          </p>
+          <div class="contain right">
+            <div class="content">
+              <h4>H.S.C</h4>
+              <h5>Savar Cantonment Public College</h5>
+            </div>
+          </div>
+          <div class="contain right">
+            <div class="content">
+              <h4>S.S.C</h4>
+              <h5>Marine School</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="timeline">
+          <div class="contain right">
+            <div class="content">
+              <h4>Internship</h4>
+              <h5>MySoft Limited</h5>
+              <h5>Java Based Microservice System Solutions</h5>
+              <h6>03 July,2021-30 september,2021</h6>
+              <h6>Java</h6>
+              <h6>Spring Boot</h6>
+              <h6>Angular</h6>
+              <h6>MySql</h6>
+              <h6>Hibernate</h6>
+            </div>
+          </div>
+          {/* <div class="contain right">
+            <div class="content">
+              <h4>2016</h4>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div> */}
         </div>
       </div>
     </section>
@@ -39,3 +63,6 @@ const About = () => {
 };
 
 export default About;
+{
+  /*  */
+}
